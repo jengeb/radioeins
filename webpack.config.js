34dcbc9c73276493f629c6f966ba9baa1bcae278
heyarne <arne@schlueter.is>
2019-06-17 16:46:16 +0200

    Sendungsseite (#22)
    * Start with article styling
    
    * Add "Zum Beitrag" button
    
    * Blur video player as well and fix margins of teaser image / video
    
    * Fix 16/9 ratio
    
    * Remove unused self-written script
    
    * A couple more refinements on hero section
    
    * Use grid mixin for navigation width
    
    * Move styles for sendungsseite to be contained by class .layoutprogramm
    
    * Move some reused styles into _layout.scss
    
    * Make the protoype a bit more clickable
    
    * Fix "live" ticker for long song titles
    
    * Use box-sizing: border-box to make managing widths and heights easier
    
    * Finish hero section
    
    * Rename $gutter-width to $col-gap (like the css property)
    
    * Configure css minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-	cache: false,
-	entry: {
-		index: ['./src/styles/index.scss']
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist')
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		})
-	],
-	module: {
-		rules: [{
-			test: /\.(sa|sc|c)ss$/,
-			use: [
-				MiniCssExtractPlugin.loader,
-				"css-loader", // translates CSS into CommonJS
-				"postcss-loader",
-				"sass-loader" // compiles Sass to CSS, using Node Sass by default
-			]
-		}]
-	}
-};
+  cache: false,
+  entry: {
+    index: ['./src/styles/index.scss']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    // used by the MiniCssExtractPlugin
+    minimizer: [new OptimizeCSSAssetsPlugin()]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.(sa|sc|c)ss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader", // translates CSS into CommonJS
+        "postcss-loader", // used for autoprefixer
+        "sass-loader" // compiles Sass to CSS, using Node Sass by default
+      ]
+    }]
+  }
+}
