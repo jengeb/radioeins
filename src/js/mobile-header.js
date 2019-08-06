483c11d98ddbc65e74060ce60488dea9881127e7
heyarne <arne@schlueter.is>
2019-08-06 14:46:47 +0200

    Implement the first couple of mobile links

--- src/js/mobile-header.js
+++ src/js/mobile-header.js
@@ -2,7 +2,9 @@ import { h, render } from 'preact'
 import { useState } from 'preact/hooks'
 
 import openNavLinksSvg from '../assets/mobile/ico-burger-black-mobile.svg'
-import closeNavLinksSvg from '../assets/mobile/ico-close-black-mobile.svg'
+import closeNavLinksSvg from '../assets/mobile/ico-close-mobile.svg'
+
+// TODO: Livestream, Tippspiel, playlist, search
 
 /**
  * Builds the mobile navigation based on what the active navi looks like
@@ -10,7 +12,7 @@ import closeNavLinksSvg from '../assets/mobile/ico-close-black-mobile.svg'
  *
  * @returns {VDom}
  */
-function Navigation ({ headerImgSrc, headerHref }) {
+function Navigation ({ headerImgSrc, headerHref, naviLinks }) {
   const [visibleSection, setVisibleSection] = useState(null)
 
   const openSection = section => e => {
@@ -21,26 +23,35 @@ function Navigation ({ headerImgSrc, headerHref }) {
 
   return <nav className='mobile-nav'>
     <div className='mobile-nav-bar'>
-      <a href='#' className='mobile-nav-burger-link' onClick={openSection('mobile-nav-links')}>
+      <a
+        href='#'
+        className={`mobile-nav-burger-button ${visibleSection === 'mobile-nav-links' && 'mobile-nav-button-open'}`}
+        onClick={openSection('mobile-nav-links')}>
         <img
           src={visibleSection === 'mobile-nav-links' ? closeNavLinksSvg : openNavLinksSvg}
           title='Navigation' />
       </a>
       <a className='mobile-nav-header-img' href={headerHref}><img src={headerImgSrc} /></a>
-      <a className='mobile-nav-live-button' href='#' onClick={openSection('mobile-nav-live')}>Live</a>
+      <a className={`mobile-nav-live-button ${visibleSection === 'mobile-nav-live' && 'mobile-nav-button-open'}`} href='#' onClick={openSection('mobile-nav-live')}>Live</a>
     </div>
 
     {/* Navigation links */}
-    <section className={`mobile-nav-section mobile-nav-links ${visibleSection === 'mobile-nav-links' || 'mobile-nav-hidden'}`}>
-      <h1>LINKS</h1>
+    <section className={`mobile-nav-section ${visibleSection === 'mobile-nav-links' || 'mobile-nav-hidden'}`}>
+      <ul className='mobile-nav-links'>
+        {/* The reason this isn't hardcoded is so we can swap out items / links more easily */}
+        {naviLinks.map(([label, href]) =>
+          <li className='mobile-nav-link-item'><a href={href}>{label}</a></li>)}
+      </ul>
     </section>
 
-    {/* Live section */}
+    {/* TODO: Live section */}
     <section className={`mobile-nav-section mobile-nav-live ${visibleSection === 'mobile-nav-live' || 'mobile-nav-hidden'}`}>
       <h1>LIVE</h1>
     </section>
   </nav>
 }
+
+const attr = (node, selector, attribute) => node.querySelector(selector).getAttribute(attribute)
 
 /**
  * Constructs the mobile navigation and mounts it after our current navigation
@@ -51,13 +62,18 @@ export function setup () {
 
   const headerNode = document.querySelector('#header')
   const navigationNode = document.querySelector('#navigationmain')
-  const liveStream = navigationNode.querySelector('#livestream')
 
   navigationNode.insertAdjacentElement('afterend', mobileNavContainer)
   render(
     <Navigation
-      headerImgSrc={headerNode.querySelector('#headerimage').getAttribute('src')}
-      headerHref={headerNode.querySelector('#headerMap area').getAttribute('href')} />,
+      headerImgSrc={attr(headerNode, '#headerimage', 'src')}
+      headerHref={attr(headerNode, '#headerMap area', 'href')}
+      naviLinks={[
+        ['Programm', attr(navigationNode, 'a[title=Programm]', 'href')],
+        ['Videos', attr(navigationNode, 'a[title=Videos]', 'href')],
+        ['Musik', attr(navigationNode, 'a[title=Musik]', 'href')],
+        ['Podcasts', attr(navigationNode, 'a[title=Podcasts]', 'href')]
+      ]} />,
     mobileNavContainer
   )
 }
